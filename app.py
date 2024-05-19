f8fe1c689305ee24851237e4a9482a5a344acb1f
Me <nikola.neykov@gmail.com>
2024-05-19 22:51:03 +0300

    Separated stores' endpoints' code, added blueprint

--- app.py
+++ app.py
@@ -3,50 +3,9 @@ import uuid
 from flask import Flask, request
 from flask_smorest import abort
 
-from db import items, stores
+from db import items
 
 app = Flask(__name__)
-
-
-@app.get('/store/<string:store_id>')
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message='Store not found.')
-
-
-@app.get('/store')
-def get_stores():
-    return {'stores': list(stores.values())}
-
-
-@app.post('/store')
-def create_store():
-    store_data = request.get_json()
-    if 'name' not in store_data:
-        abort(
-            400,
-            message="Bad request. Ensure 'name' is included in the JSON payload.",
-        )
-    for store in stores.values():
-        if store_data['name'] == store['name']:
-            abort(400, message='Store already exists.')
-
-    store_id = uuid.uuid4().hex
-    store = {**store_data, 'id': store_id}
-    stores[store_id] = store
-
-    return store
-
-
-@app.delete('/store/<string:store_id>')
-def delete_store(store_id):
-    try:
-        del stores[store_id]
-        return {'message': 'Store deleted.'}
-    except KeyError:
-        abort(404, message='Store not found.')
 
 
 @app.get('/item')
@@ -105,7 +64,7 @@ def update_item(item_id):
         abort(404, message='Item not found.')
 
 
-@app.delete('/item/<string:item_id')
+@app.delete('/item/<string:item_id>')
 def delete_item(item_id):
     try:
         del items[item_id]
