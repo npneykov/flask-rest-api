9ab356366afed01712fe74bdaa2881c6e15973e5
npneykov <nikola.neykov@gmail.com>
2024-05-23 22:55:43 +0300

    Modified item/store methods to include validations

--- src/item.py
+++ src/item.py
@@ -5,6 +5,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 from db import items
+from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint('Items', __name__, description='Operations on items')
 
@@ -24,16 +25,10 @@ class Item(MethodView):
         except KeyError:
             abort(404, message='Item not found.')
 
-    def put(self, item_id):
-        item_data = request.get_json()
-        if 'price' not in item_data or 'name' not in item_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
-            )
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
         try:
             item = items[item_id]
-
             item |= item_data
 
             return item
@@ -46,17 +41,8 @@ class ItemList(MethodView):
     def get(self):
         return {'items': list(items.values())}
 
-    def post(self):
-        item_data = request.get_json()
-        if (
-            'price' not in item_data
-            or 'store_id' not in item_data
-            or 'name' not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-            )
+    @blp.arguments(ItemSchema)
+    def post(self, item_data):
         for item in items.values():
             if (
                 item_data['name'] == item['name']
