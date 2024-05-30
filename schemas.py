928f6f72bd0e5330368ace214441dce4ce737008
npneykov <nikola.neykov@gmail.com>
2024-05-30 23:21:19 +0300

    Added tag model, modified schemas classes

--- schemas.py
+++ schemas.py
@@ -1,18 +1,19 @@
 from marshmallow import Schema, fields
 
 
-class ItemSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
-    price = fields.Float(required=True)
-    store_id = fields.Int(required=True)
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 
-class ItemUpdateSchema(Schema):
-    name = fields.Str()
-    price = fields.Float()
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
 
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
