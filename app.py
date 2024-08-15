e85a405001e9a761ac5814c75ecd10ee40537de7
npneykov <nikola.neykov@gmail.com>
2024-08-15 15:00:44 +0300

    Added many to many relationships, also made configuration changes

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-import os
-
 from flask import Flask
 from flask_smorest import Api
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
 
 import models
 from db import db
-from src.item import blp as ItemBlueprint
-from src.store import blp as StoreBlueprint
 
 
 def create_app(db_url=None):
     app = Flask(__name__)
-    app.config['PROPAGATE_EXCEPTIONS'] = True
     app.config['API_TITLE'] = 'Stores REST API'
     app.config['API_VERSION'] = 'v1'
     app.config['OPENAPI_VERSION'] = '3.0.3'
@@ -20,10 +18,9 @@ def create_app(db_url=None):
     app.config['OPENAPI_SWAGGER_UI_URL'] = (
         'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
     )
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or os.getenv(
-        'DATABASE_URL', 'sqlite:///data.db'
-    )
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or 'sqlite:///data.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['PROPAGATE_EXCEPTIONS'] = True
     db.init_app(app)
     api = Api(app)
 
@@ -32,5 +29,6 @@ def create_app(db_url=None):
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
 
     return app
--- src/item.py
+++ src/item.py
@@ -1,12 +1,9 @@
-import uuid
-
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 
-from db import db, items
-from models.item import ItemModel
+from db import db
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint('Items', __name__, description='Operations on items')
@@ -14,35 +11,41 @@ blp = Blueprint('Items', __name__, description='Operations on items')
 
 @blp.route('/item/<string:item_id>')
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message='Item not found.')
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {'message': 'Item deleted.'}
-        except KeyError:
-            abort(404, message='Item not found.')
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {'message': 'Item deleted.'}
 
     @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        try:
-            item = items[item_id]
-            item |= item_data
+        item = ItemModel.query.get(item_id)
 
-            return item
-        except KeyError:
-            abort(404, message='Item not found.')
+        if item:
+            item.price = item_data['price']
+            item.name = item_data['name']
+        else:
+            item = ItemModel(id=item_id, **item_data)
+
+        db.session.add(item)
+        db.session.commit()
+
+        return item
 
 
 @blp.route('/item')
 class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {'items': list(items.values())}
+        return ItemModel.query.all()
 
+    @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
         item = ItemModel(**item_data)
