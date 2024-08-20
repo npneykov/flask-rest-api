5b6caeffa197f0b8e8cd913385525dc2c2151b71
npneykov <nikola.neykov@gmail.com>
2024-08-20 14:59:22 +0300

    Added jwt authentication requirement for endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
 from flask_smorest import Api
 
@@ -27,6 +27,34 @@ def create_app(db_url=None):
     db.init_app(app)
     api = Api(app)
     jwt = JWTManager(app)
+
+    @jwt.invalid_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({'message': 'The token has expired.', 'error': 'token_expired'}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error_string):
+        return (
+            jsonify(
+                {'message': 'Signature verification failed.', 'error': 'invalid_token'}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error_string):
+        return (
+            jsonify(
+                {
+                    'description': 'Request does not contain an access token.',
+                    'error': 'authorization_required',
+                }
+            ),
+            401,
+        )
 
     with app.app_context():
         db.create_all()
