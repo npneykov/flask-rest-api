bd394a6398bcf9bd7c34f20b4fcde85bfecebf8e
npneykov <nikola.neykov@gmail.com>
2024-08-21 20:25:52 +0300

    Added jwt fresh token functionality

--- src/user.py
+++ src/user.py
@@ -1,5 +1,11 @@
 from flask.views import MethodView
-from flask_jwt_extended import create_access_token, get_jwt, jwt_required
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    get_jwt,
+    get_jwt_identity,
+    jwt_required,
+)
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 
@@ -20,10 +26,22 @@ class UserLogin(MethodView):
         ).first()
 
         if user and pbkdf2_sha256.verify(user_data['password'], user.password):
-            access_token = create_access_token(identity=user.id)
-            return {'access_token': access_token}, 200
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(identity=user.id)
+            return {'access_token': access_token, 'refresh_token': refresh_token}, 200
 
         abort(401, message='Invalid credentials')
+
+
+@blp.route('/refresh')
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        jti = get_jwt()['jti']
+        BLOCKLIST.add(jti)
+        return {'access_token': new_token}, 200
 
 
 @blp.route('/logout')
