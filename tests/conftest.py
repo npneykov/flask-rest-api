c72f221cbed95eca43c8350773741c4e0a8c957b
npneykov <nikola.neykov@gmail.com>
2025-01-23 15:09:02 +0200

    Added conftest.py for pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+
+import db
+from app import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app(testing=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['TESTING'] = True
+
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
