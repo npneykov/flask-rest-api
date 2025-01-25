68032bb44029543ce692ab16da18e591bd11471d
Me <nikola.neykov@gmail.com>
2025-01-25 16:31:09 +0200

    Modified conftest/__init__.py in tests folder

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+# Get the parent directory path
+parent_dir = os.path.dirname(os.path.dirname(__file__))
+
+# Insert the parent directory into the system path
+sys.path.insert(0, parent_dir)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,18 @@
 import pytest
 
-import db
+from db import db
 from app import create_app
 
 
 @pytest.fixture
 def app():
-    app = create_app(testing=True)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app = create_app()
     app.config['TESTING'] = True
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
     with app.app_context():
         db.create_all()
-        yield app
-        db.session.remove()
+    yield app
+    with app.app_context():
         db.drop_all()
 
 
