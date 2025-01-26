9df3acc77a694bf05b5d8a913aa63ac96a09734c
Me <nikola.neykov@gmail.com>
2025-01-26 17:56:28 +0200

    Fixed item tests, removed jwt auth temporarily

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,36 +1,34 @@
-import logging
-
-
 def test_post_item(client):
     """Check if the item is added to the database"""
-    logging.debug(
-        client.post('/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': 1})
+    response = client.post(
+        '/item',
+        json={'name': 'Test Item', 'price': 10.99, 'store_id': 1},
     )
+    assert response.status_code == 201
 
 
 def test_get_item(client):
     """Check if the response is a dictionary"""
     client.post('/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': 1})
-    logging.debug(isinstance(client.get('/item/1').json, dict))
+    assert isinstance(client.get('/item/1').json, dict)
 
 
 def test_get_all_items(client):
     """CHeck if the response is a list"""
     client.post('/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': 1})
     client.post('/item', json={'name': 'Test Item2', 'price': 15.99, 'store_id': 1})
-    logging.debug(isinstance(client.get('/item').json, (list, dict)))
+    assert isinstance(client.get('/item').json, list)
 
 
 def test_delete_item(client):
     """Check if the item is deleted from the database"""
     client.post('/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': 1})
-    client.delete('/item/1')
-    logging.debug(client.get('/item/1').status_code == 404)
+    response = client.delete('/item/1')
+    assert response.status_code == 200
 
 
 def test_put_item(client):
     """Check if the item is updated in the database"""
-    client.post('/store', json={'store_id': 1, 'name': 'Test Store'})
     client.post('/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': 1})
-    client.put('/item/1', json={'name': 'Test Item', 'price': 15.99, 'store_id': 1})
-    assert client.get('/item/1').json.get('price') == 15.99
+    response = client.put('/item/1', json={'name': 'Test Item', 'price': 15.99})
+    assert response.json.get('price') == 15.99
