5a4ae5109ac653857705dc5bcccc594e3aa5648c
Me <nikola.neykov@gmail.com>
2025-02-09 22:45:29 +0200

    Reorganized tests in separate classes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,141 +1,133 @@
-def test_post_item(client):
-    """Check if the item is added to the database"""
-    response = client.post('/store', json={'name': 'Test Store'})
-    store_id = response.json.get('id')
-    response = client.post(
-        '/item',
-        json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id},
-    )
-    assert response.status_code == 201
+class TestItems:
+    def test_post_item(client):
+        """Check if the item is added to the database"""
+        response = client.post('/store', json={'name': 'Test Store'})
+        store_id = response.json.get('id')
+        response = client.post(
+            '/item',
+            json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id},
+        )
+        assert response.status_code == 201
+
+    def test_get_item(client):
+        """Check if the response is a dictionary"""
+        response = client.post('/store', json={'name': 'Test Store'})
+        store_id = response.json.get('id')
+        client.post(
+            '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
+        )
+        assert isinstance(client.get('/item/1').json, dict)
+
+    def test_get_all_items(client):
+        """CHeck if the response is a list"""
+        response = client.post('/store', json={'name': 'Test Store'})
+        store_id = response.json.get('id')
+        client.post(
+            '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
+        )
+        client.post(
+            '/item', json={'name': 'Test Item2', 'price': 15.99, 'store_id': store_id}
+        )
+        assert isinstance(client.get('/item').json, list)
+
+    def test_delete_item(client):
+        """Check if the item is deleted from the database"""
+        response = client.post('/store', json={'name': 'Test Store'})
+        store_id = response.json.get('id')
+        client.post(
+            '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
+        )
+        response = client.delete('/item/1')
+        assert response.status_code == 200
+
+    def test_put_item(client):
+        """Check if the item is updated in the database"""
+        response = client.post('/store', json={'name': 'Test Store'})
+        store_id = response.json.get('id')
+        client.post(
+            '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
+        )
+        response = client.put('/item/1', json={'name': 'Test Item', 'price': 15.99})
+        assert response.json.get('price') == 15.99
 
 
-def test_get_item(client):
-    """Check if the response is a dictionary"""
-    response = client.post('/store', json={'name': 'Test Store'})
-    store_id = response.json.get('id')
-    client.post(
-        '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
-    )
-    assert isinstance(client.get('/item/1').json, dict)
+class TestStore:
+    def test_post_store(client):
+        """Check if the store is added to the database"""
+        response = client.post('/store', json={'name': 'Test Store'})
+        assert response.status_code == 201
+
+    def test_get_all_stores(client):
+        """Check if the response is a dictionary"""
+        client.post('/store', json={'name': 'Test Store'})
+        client.post('/store', json={'name': 'Test Store2'})
+        client.post('/store', json={'name': 'Test Store3'})
+        assert isinstance(client.get('/store').json, list)
+
+    def test_delete_store(client):
+        """Check if the store is deleted from the database"""
+        response = client.post('/store', json={'name': 'Test Store'})
+        store_id = response.json.get('id')
+        response = client.delete(f'/store/{store_id}')
+        assert response.status_code == 200
 
 
-def test_get_all_items(client):
-    """CHeck if the response is a list"""
-    response = client.post('/store', json={'name': 'Test Store'})
-    store_id = response.json.get('id')
-    client.post(
-        '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
-    )
-    client.post(
-        '/item', json={'name': 'Test Item2', 'price': 15.99, 'store_id': store_id}
-    )
-    assert isinstance(client.get('/item').json, list)
+class TestTags:
+    def test_get_tags_from_store(client):
+        """Check if the response is a list"""
+        response = client.post('/store', json={'name': 'Test Store'})
+        store_id = response.json.get('id')
+        response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
+        assert isinstance(client.get(f'/store/{store_id}/tag').json, list)
 
+    def test_post_tags_to_store(client):
+        """Check if the tag is added to the database"""
+        response = client.post('/store', json={'name': 'Test Store'})
+        store_id = response.json.get('id')
+        response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
+        assert response.status_code == 201
 
-def test_delete_item(client):
-    """Check if the item is deleted from the database"""
-    response = client.post('/store', json={'name': 'Test Store'})
-    store_id = response.json.get('id')
-    client.post(
-        '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
-    )
-    response = client.delete('/item/1')
-    assert response.status_code == 200
+    def test_post_tag_to_item(client):
+        """Check if the tag is added to the database"""
+        store_response = client.post('/store', json={'name': 'Test Store'})
+        store_id = store_response.json.get('id')
+        item_response = client.post(
+            '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
+        )
+        item_id = item_response.json.get('id')
+        tag_response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
+        tag_id = tag_response.json.get('id')
+        response = client.post(f'/item/{item_id}/tag/{tag_id}')
+        assert response.status_code == 201
 
+    def test_delete_tag_from_item(client):
+        """Check if the tag is deleted from the database"""
+        store_response = client.post('/store', json={'name': 'Test Store'})
+        store_id = store_response.json.get('id')
+        item_response = client.post(
+            '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
+        )
+        item_id = item_response.json.get('id')
+        tag_response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
+        tag_id = tag_response.json.get('id')
+        client.post(f'/item/{item_id}/tag/{tag_id}')
+        response = client.delete(f'/item/{item_id}/tag/{tag_id}')
+        assert response.status_code == 200
 
-def test_put_item(client):
-    """Check if the item is updated in the database"""
-    response = client.post('/store', json={'name': 'Test Store'})
-    store_id = response.json.get('id')
-    client.post(
-        '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
-    )
-    response = client.put('/item/1', json={'name': 'Test Item', 'price': 15.99})
-    assert response.json.get('price') == 15.99
+    def test_get_tag(client):
+        """Check if the tag is added to the database"""
+        store_response = client.post('/store', json={'name': 'Test Store'})
+        store_id = store_response.json.get('id')
+        tag_response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
+        tag_id = tag_response.json.get('id')
+        response = client.get(f'/tag/{tag_id}')
+        assert response.status_code == 200
 
-
-def test_post_store(client):
-    """Check if the store is added to the database"""
-    response = client.post('/store', json={'name': 'Test Store'})
-    assert response.status_code == 201
-
-
-def test_get_all_stores(client):
-    """Check if the response is a dictionary"""
-    client.post('/store', json={'name': 'Test Store'})
-    client.post('/store', json={'name': 'Test Store2'})
-    client.post('/store', json={'name': 'Test Store3'})
-    assert isinstance(client.get('/store').json, list)
-
-
-def test_delete_store(client):
-    """Check if the store is deleted from the database"""
-    response = client.post('/store', json={'name': 'Test Store'})
-    store_id = response.json.get('id')
-    response = client.delete(f'/store/{store_id}')
-    assert response.status_code == 200
-
-
-def test_get_tags_from_store(client):
-    """Check if the response is a list"""
-    response = client.post('/store', json={'name': 'Test Store'})
-    store_id = response.json.get('id')
-    response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
-    assert isinstance(client.get(f'/store/{store_id}/tag').json, list)
-
-
-def test_post_tags_to_store(client):
-    """Check if the tag is added to the database"""
-    response = client.post('/store', json={'name': 'Test Store'})
-    store_id = response.json.get('id')
-    response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
-    assert response.status_code == 201
-
-
-def test_post_tag_to_item(client):
-    """Check if the tag is added to the database"""
-    store_response = client.post('/store', json={'name': 'Test Store'})
-    store_id = store_response.json.get('id')
-    item_response = client.post(
-        '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
-    )
-    item_id = item_response.json.get('id')
-    tag_response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
-    tag_id = tag_response.json.get('id')
-    response = client.post(f'/item/{item_id}/tag/{tag_id}')
-    assert response.status_code == 201
-
-
-def test_delete_tag_from_item(client):
-    """Check if the tag is deleted from the database"""
-    store_response = client.post('/store', json={'name': 'Test Store'})
-    store_id = store_response.json.get('id')
-    item_response = client.post(
-        '/item', json={'name': 'Test Item', 'price': 10.99, 'store_id': store_id}
-    )
-    item_id = item_response.json.get('id')
-    tag_response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
-    tag_id = tag_response.json.get('id')
-    client.post(f'/item/{item_id}/tag/{tag_id}')
-    response = client.delete(f'/item/{item_id}/tag/{tag_id}')
-    assert response.status_code == 200
-
-
-def test_get_tag(client):
-    """Check if the tag is added to the database"""
-    store_response = client.post('/store', json={'name': 'Test Store'})
-    store_id = store_response.json.get('id')
-    tag_response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
-    tag_id = tag_response.json.get('id')
-    response = client.get(f'/tag/{tag_id}')
-    assert response.status_code == 200
-
-
-def test_delete_tag(client):
-    """Check if the tag is deleted from the database"""
-    store_response = client.post('/store', json={'name': 'Test Store'})
-    store_id = store_response.json.get('id')
-    tag_response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
-    tag_id = tag_response.json.get('id')
-    response = client.delete(f'/tag/{tag_id}')
-    assert response.status_code == 200
+    def test_delete_tag(client):
+        """Check if the tag is deleted from the database"""
+        store_response = client.post('/store', json={'name': 'Test Store'})
+        store_id = store_response.json.get('id')
+        tag_response = client.post(f'/store/{store_id}/tag', json={'name': 'Test Tag'})
+        tag_id = tag_response.json.get('id')
+        response = client.delete(f'/tag/{tag_id}')
+        assert response.status_code == 200
